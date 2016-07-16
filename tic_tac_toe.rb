5bf57da42b8bb68d09a7b6db8dcad0c61a4cad09
Shadman <shadman.jamil@tenpearls.com>
2016-07-17 01:57:41 +0500

    switching turns

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,13 @@
 
 class TicTacToe
-	attr_accessor :players_option, :game_option
+	attr_accessor :players_option, :game_option, :user_turn, :total_actions
 
 	
 	def initialize
 		@players_option
 		@game_option
+		@user_turn = 'X'
+		@total_actions = 0
 	end
 
 	def player_options
@@ -36,15 +38,48 @@ class TicTacToe
 		selected
 	end
 
+
+	def start(playerX, playerY)
+		self.draw_board
+		player = play_and_switch(playerX, playerY)
+		if is_finished === false
+			self.play(player)
+			start(playerX, playerY)
+		end
+	end 
+
+	def play_and_switch(playerX, playerY)
+		if self.user_turn == 'Y'
+			self.user_turn = 'X'
+			playerY
+		else
+			self.user_turn = 'Y'
+			playerX
+		end
+	end
+
+	def is_finished
+		game_option = self.game_option * self.game_option
+		if self.total_actions == game_option
+			puts 'Game Draw !!'
+			return true
+		end
+		return false
+	end
+
 	def draw_header
 		line = ''
-		max_col = 20*@game_option
+		max_col = 20 * self.game_option
 		(0..max_col).each do |a|
 			line += '='
 		end
 
 		puts "\n" + line
-		puts "\t\tTic Tac Toe Game Started   "
+		if @total_actions == 0
+			puts "\t\tTic Tac Toe Game Started   "
+		else 
+			puts "\t\tTic Tac Toe Game (#{@total_actions} Turn)   "
+		end
 		puts line + "\n\n"
 	end 
 
@@ -53,26 +88,25 @@ class TicTacToe
 
 		count = 1
 		string = ''
-		(1..@game_option).each do |i|
+		(1..self.game_option).each do |i|
 			
 			string = "\t\t\t\n"
-			(1..@game_option).each do |y|
+			(1..self.game_option).each do |y|
 				string += "\t|\t" + count.to_s
 				count += 1
 			end
 			puts string + "\t|\n\n"
 		end
-
-		self.play
 	end 
 
-	def play
+	def play(player)
 		option = 0
-		game_option = @game_option*@game_option
+		game_option = self.game_option * self.game_option
 		while !(1..game_option).include?(option.to_i)
-			puts "\n\nPlease select your desired position from 1 to #{game_option}:"
+			puts "\n\n" + player.name + ": Please select your desired position from 1 to #{game_option}:"
 			option = gets.chomp
 		end	
+		self.total_actions += 1
 		option
 	end
 
@@ -80,11 +114,13 @@ end
 
 
 class Player
-	attr_accessor :name, :score
+	attr_accessor :name, :score, :user_type, :actions
 	
-	def initialize(name)
+	def initialize(name, user_type)
 		@name = name
 		@score = 0
+		@user_type
+		@actions
 	end
 
 	def score_increase
@@ -102,18 +138,21 @@ class Play
 		game_players = selections.player_options
 		selections.game_options
 
+		# Player 1 initialization
 		puts "\n1st Player Name: "
-		playerX = Player.new(gets.chomp)
+		playerX = Player.new(gets.chomp, 'X')
 
+		# Player 2 initialization
 		if game_players == 1
 			puts "\n2nd Player Name: "
-			playerY = Player.new(gets.chomp)
+			playerY = Player.new(gets.chomp, 'Y')
 		else 
 			puts "\nComputer Initialized. "
-			playerY = Player.new('Computer')
+			playerY = Player.new('Computer', 'Y')
 		end
 
-		selections.draw_board
+		# Drawing board as per option selection
+		selections.start(playerX, playerY)
 	end
 
 end
